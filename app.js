25a47df8e3ab19e79d34cfd3b2aaeea28c498742
Brian Harney <brian.r.harney@gmail.com>
2020-12-10 18:27:04 -0600

    finish challenge 12

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+const posts = [];
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -19,7 +21,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { homeStartingContent: homeStartingContent });
+  res.render("home", { posts: posts });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactStartingContent: contactContent });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutStartingContent: aboutContent });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  let post = {
+    title: req.body.postTitle,
+    body: req.body.postBody,
+  };
+  posts.push(post);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
